7b5c55623d84b860780bfeacba834d30ed516926
jcoop32 <jcooper11@hawk.iit.edu>
2023-07-07 11:23:20 -0500

    details page added

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('collection/', views.collection, name='collection'),
+    path('collection/<int:shoe_id>/', views.shoe_details, name='details'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,7 @@ def about(request):
 def collection(request):
     shoes = Shoe.objects.all()  # Assuming each shoe is associated with a user
     return render(request, 'shoes/collection.html', {'shoes': shoes})
+
+def shoe_details(request, shoe_id):
+    shoe = Shoe.objects.get(id=shoe_id)
+    return render(request, 'shoes/detail.html', {'shoe': shoe})
