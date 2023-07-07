a908a2dc4a0acf5e322a2428c77d863e1ec49e1f
jcoop32 <jcooper11@hawk.iit.edu>
2023-07-07 10:28:51 -0500

    collectionp page added

--- main_app/models.py
+++ main_app/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class Shoe(models.Model):
     brand = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
+    modelName = models.CharField(max_length=100)
+    colorway = models.CharField(max_length=100)
     description = models.TextField(max_length=200)
     year = models.IntegerField('Year Released')
     price = models.FloatField()
-def __str__(self):
-    return f'{self.brand} {self.model}'
+    
+    def __str__(self):
+        return f'{self.brand} ({self.modelName})'
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Shoe
 
 # Create your views here.
 def home(request):
@@ -6,3 +7,7 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def collection(request):
+    shoes = Shoe.objects.all()  # Assuming each shoe is associated with a user
+    return render(request, 'shoes/collection.html', {'shoes': shoes})
