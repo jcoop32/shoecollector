41bbc5f23cd7d33f15f515d67877268868452dba
jcoop32 <jcooper11@hawk.iit.edu>
2023-07-07 09:49:47 -0500

    model created

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Shoe(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    description = models.TextField(max_length=200)
+    year = models.IntegerField('Year Released')
+    price = models.FloatField()
+def __str__(self):
+    return f'{self.brand} {self.model}'
